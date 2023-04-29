36ca9a20ae26ac159ae3a46a4a6935099c67f161
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-04-29 10:59:11 -0400

    updated /dist

--- server/server.jsx
+++ server/server.jsx
@@ -4,6 +4,7 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const  createCheckoutSession  = require('./checkout')
+const path = require('path')
 
 const PORT = process.env.PORT
 //middleware
@@ -11,6 +12,15 @@ app.use(express.static("../client"))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 //GET config
 app.get("/config", (req, res) => res.send({ publishableKey: process.env.STRIPE_PUBLISHABLE_KEY }))
