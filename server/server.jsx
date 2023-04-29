e94247a87c2bfe3ee57e829742870f6986bae88d
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-04-29 11:04:06 -0400

    fixed home route

--- server/server.jsx
+++ server/server.jsx
@@ -18,8 +18,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+})
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 //GET config
