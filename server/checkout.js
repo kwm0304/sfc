3da45b406a66440886830c2440bf68567c9789a3
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-05-02 15:26:17 -0400

    changes

--- server/checkout.js
+++ server/checkout.js
@@ -2,7 +2,7 @@ const stripeAPI = require('./stripe');
 
 async function createCheckoutSession(req, res) {
     //the web app url will be the business url customers will be redirected to after checkout
-    const { line_items, customer_email, amount } = req.body;
+    const { line_items, customer_email } = req.body;
     console.log(req.body)
     //check req body has line items and email
     if(!line_items || !customer_email) {
@@ -19,11 +19,11 @@ async function createCheckoutSession(req, res) {
             customer_email, 
             
             //will be the url the deployed app will be linked to
-            success_url: 'https://soles-for-christ.herokuapp.com/',
-            cancel_url: 'https://soles-for-christ.herokuapp.com/',
+            success_url: 'https://soles-for-christ.herokuapp.com',
+            cancel_url: 'https://soles-for-christ.herokuapp.com',
             submit_type: 'donate'
         });
-        res.status(200).json({ sessionId: session.id, }) && res.redirect(303, session.url)
+        res.status(200).json({ sessionId: session.id }) && res.redirect(303, session.url)
     } catch (error) {
         res.status(400).json({ error: 'an error occured, unable to create session'})
     }
