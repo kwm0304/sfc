818c150eee88b898365ed7cafa0b7883e9c57d34
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-05-03 08:44:28 -0400

    logging stuff

--- client/src/components/PaymentForm.jsx
+++ client/src/components/PaymentForm.jsx
@@ -18,7 +18,7 @@ export default function PaymentForm() {
    })
 
    const { sessionId } = response;
-   console.log({sessionId})
+   console.log(typeof { sessionId })
    const { error } = await stripe.redirectToCheckout({ sessionId });
    console.log('response', response)
 
