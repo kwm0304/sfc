0a8801b86628c194a9131d11c4a3f2c70c82a31e
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-05-03 13:39:30 -0400

    leaving headers out of post to see if sid gets passed

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -6,9 +6,7 @@ export async function fetchFromAPI(endpoint, opts) {
   const res = await fetch(`https://soles-for-christ.herokuapp.com/${endpoint}`, {
     method,
     ...(body && { body: JSON.stringify(body) }),
-    headers: {
-      'Content-Type': 'application/json'
-    }
+    
   });
   return res.json()
 }
