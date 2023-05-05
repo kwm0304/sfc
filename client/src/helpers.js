3897eeda9c7acd0c903f316a0f1d927ffad49920
kwm0304 <106484883+kwm0304@users.noreply.github.com>
2023-05-04 20:34:52 -0400

    fgd

--- client/src/helpers.js
+++ client/src/helpers.js
@@ -1,6 +1,6 @@
 export async function fetchFromAPI(endpoint, opts) {
   const { method, body } = { method: "POST", body: null, ...opts};
-
+  console.log("Fetch running")
   const res = await fetch(`https://soles-for-christ.herokuapp.com/${endpoint}`, {
     method,
     ...(body && { body: JSON.stringify(body) }),
